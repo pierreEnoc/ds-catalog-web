9aada02bea797bf35ab8e8105b9fd9d7aa80c94c
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-06 22:07:16 -0300

    :sparkles: start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        console.log('componente iniciando')
-    },[]);
-
-    return (
-        <div className="container mt-5">
-            <button 
-                className="btn btn-primary mr-5"
-                onClick={() => setCounter(counter + 1)}
-            >
-                +
-            </button>
-            <span>
-               {counter}
-            </span>
-            <button 
-                className="btn btn-primary ml-5"
-                onClick={() => setCounter(counter - 1)}
-            >
-                -
-            </button> 
-            {counter > 5 && <h1> o valor é maior que 5</h1>} 
-            {counter <= 5 && <h1> o valor é nenor que 5</h1>} 
-           
-        </div>
-       
+    return(
+    <h1>App</h1>
     );
 }
 
