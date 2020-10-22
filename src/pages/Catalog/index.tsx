2e08d62efdfb5986f8e1e34664838248642152f0
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-22 14:18:36 -0300

    :recycle:refactor loader

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
+// import ProductCardLoader from './components/ProductCardLoader';
 import './styles.scss';
 
-const Ctalog = () => {
 
+const Ctalog = () => {
     const [ productsResponse, setProductsResponse] = useState<ProductsResponse> ();
     console.log(productsResponse);
-
     useEffect(() => {
         const params ={
             page: 0,
             linesPerPage: 12
         }
-
         makeRequest({ url: '/products', params })
            .then(response => setProductsResponse(response.data));
     }, [])
-
     return(
         <div className="catalog-container">
         <h1 className="catalog-title">
@@ -27,7 +25,7 @@ const Ctalog = () => {
         </h1>
         <div className="catalog-products">
              {productsResponse?.content.map (product => (
-                 <Link to={`/products/${product.id}`} key={product.id}>
+                <Link to={`/products/${product.id}`} key={product.id}>
                      <ProductCard product={product} />
                  </Link>
              ))}
