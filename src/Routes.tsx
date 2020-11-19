bd9c0ac05346953c4abc5eadb17375b871eff8ad
pierreEnoc <pierre_enoc@yahoo.fr>
2020-11-19 18:23:39 -0300

    :redirecting the user to login after the backend returns 401 in a request

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { Router, Switch, Route, Redirect } from 'react-router-dom'
 import Home from './pages/Home';
 import Catalog from './pages/Catalog'
 import Admin from './pages/Admin';
 import Navbar from './core/components/Navbar';
 import ProductDetails from './core/components/ProductDetails';
 import Auth from './pages/Auth';
+import history from './core/utils/history';
 
 const Routes = () => (
-    <BrowserRouter>
+    <Router history={history}>
         <Navbar />
         <Switch>
             <Route path="/" exact>
@@ -31,7 +32,7 @@ const Routes = () => (
 
         </Switch>
 
-    </BrowserRouter>
+    </Router>
 );
 
 export default Routes;
