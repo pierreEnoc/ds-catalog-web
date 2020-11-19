005413d430df13b3109d81c3c30c3f4594211302
pierreEnoc <pierre_enoc@yahoo.fr>
2020-11-18 21:41:52 -0300

    :sparkles: config biblitec react-hook-from

--- src/pages/Auth/components/Login/index.tsx
+++ src/pages/Auth/components/Login/index.tsx
@@ -1,22 +1,39 @@
 import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    password: string;
+}
+
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>(); 
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    }
+
     return (
         <AuthCard  title="login">
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <input 
             type="email" 
             className="form-control input-base margin-botton-30"
             placeholder="Email" 
+            name="email" 
+            ref={register}
+            
         />
          <input 
             type="password" 
             className="form-control input-base"
             placeholder="senha" 
+            name="password" 
+            ref={register}
         />
         <Link to="/admin/auth/recover" className="login-link-recover"> 
             Esqueci a senha?
