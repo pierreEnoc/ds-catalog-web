c9bcc7e1626a2bb3bd03f2167b9679b973920d65
pierreEnoc <pierre_enoc@yahoo.fr>
2020-11-25 23:04:19 -0300

    :sparkles: add validation form product

--- src/pages/Admin/components/Products/Form/index.tsx
+++ src/pages/Admin/components/Products/Form/index.tsx
@@ -10,14 +10,13 @@ type FormState = {
     price: string;
     description: string;
     imageUrl: String;
-
 }
 
 const Form = () => {
-    const { register, handleSubmit } = useForm<FormState>();
+    const { register, handleSubmit, errors } = useForm<FormState>();
 
-    const onSubmit = (data: FormState ) => {
-        makePrivateRequest({ url: '/products', method: 'POST', data: data })     
+    const onSubmit = (data: FormState) => {
+        makePrivateRequest({ url: '/products', method: 'POST', data: data })
     }
 
     return (
@@ -25,39 +24,70 @@ const Form = () => {
             <BaseForm title="cadastrar um produto">
                 <div className="row">
                     <div className="col-6">
-                        <input
-                           ref={register({ required: "Campo obrigatório" })}
-                            name="name"
-                            type="text"
-                            className="form-control margin-botton-30 input-base"
-                            placeholder="Nome do produto"
+                        <div className="margin-botton-30">
+                            <input
+                                ref={register({ 
+                                    required: "Campo obrigatório",
+                                    minLength: {value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
+                                    maxLength: {value: 60, message: 'O campo deve ter no máximo 60 caracteres'}
+
+                                })}
+                                name="name"
+                                type="text"
+                                className="form-control  input-base"
+                                placeholder="Nome do produto"
+                            />
+                            {errors.name && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.name.message}
+                                </div>
+                            )}
+                        </div>
+
+                        <div className="margin-botton-30">
+                            <input
+                                ref={register({ required: "Campo obrigatório" })}
+                                name="price"
+                                type="number"
+                                className="form-control input-base"
+                                placeholder="Preço"
+                            />
+                            {errors.price && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.price.message}
+                                </div>
+                            )}
+                        </div>
+                        <div className="margin-botton-30">
+                            <input
+                                ref={register({ required: "Campo obrigatório" })}
+                                name="imageUrl"
+                                type="text"
+                                className="form-control  input-base"
+                                placeholder="imagem do produto"
                         />
-                       
-                        <input
-                            ref={register({ required: "Campo obrigatório" })}
-                            name="price"
-                            type="number"
-                            className="form-control margin-botton-30 input-base"
-                            placeholder="Preço"
-                        />
-                           <input
-                            ref={register({ required: "Campo obrigatório" })}
-                            name="imageUrl"
-                            type="text"
-                            className="form-control margin-botton-30 input-base"
-                            placeholder="imagem do produto"
-                        />
+                            {errors.imageUrl && (
+                                <div className="invalid-feedback d-block">
+                                    {(errors.imageUrl as any)?.message}
+                                </div>
+                            )}
+                        </div>
                     </div>
 
                     <div className="col-6" >
                         <textarea
                             ref={register({ required: "Campo obrigatório" })}
-                            name="descrição"
+                            name="description"
                             className="form-control input-base"
                             placeholder="Descrição"
                             cols={30}
                             rows={10}
                         />
+                         {errors.description && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.description.message}
+                                </div>
+                            )}
                     </div>
                 </div>
             </BaseForm>
