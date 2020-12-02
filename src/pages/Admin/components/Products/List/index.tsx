36526b98632ce4bf2c26239517c096a2052ca76e
pierreEnoc <pierre_enoc@yahoo.fr>
2020-12-02 18:23:54 -0300

    :sparkles: remove product

--- src/pages/Admin/components/Products/List/index.tsx
+++ src/pages/Admin/components/Products/List/index.tsx
@@ -1,33 +1,54 @@
 import Pagination from 'core/components/Pagination';
 import { ProductsResponse } from 'core/types/Product';
-import { makeRequest } from 'core/utils/request';
-import React, { useEffect, useState } from 'react';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Card from '../Card';
 
 const List = () => {
-    const [ productsResponse, setProductsResponse] = useState<ProductsResponse> ();
-    const [isLoading, setIsLoading] = useState (false);
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
     const history = useHistory();
 
-    useEffect(() => {
-        const params ={page: activePage,
+    const getProducts = useCallback(() => {
+        const params = {
+            page: activePage,
             linesPerPage: 4,
             direction: 'DESC',
             orderBy: 'id'
         }
-
         setIsLoading(true)
         makeRequest({ url: '/products', params })
-           .then(response => setProductsResponse(response.data))
-           .finally(() =>{
-            setIsLoading(false);
-           })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
     }, [activePage]);
+
+    useEffect(() => {
+        getProducts();
+    }, [getProducts]);
 
     const handleCreate = () => {
         history.push('/admin/products/create');
+    }
+
+    const onRemove = (productId: number) => {
+        const confirm = window.confirm('Deseja realmente excluir est produto?');
+
+        if (confirm) {
+            makePrivateRequest({ url: `/products/${productId}`, method: 'DELETE' })
+                .then(() => {
+                    toast.info('Produto removido com sucesso!');
+                    getProducts();
+                })
+                .catch(() => {
+                    toast.error('Error ao remover produto!');
+                })
+        }
+
     }
 
     return (
@@ -37,15 +58,15 @@ const List = () => {
             </button>
             <div className="admin-list-container">
                 {productsResponse?.content.map(product => (
-                    <Card product={product} key={product.id} />
+                    <Card product={product} key={product.id} onRemove={onRemove} />
                 ))}
-             {productsResponse && (
-             <Pagination
-              totalPages={productsResponse.totalPages} 
-              activePage={activePage}
-              onChange={page => setActivePage(page)}
-              />
-            )}
+                {productsResponse && (
+                    <Pagination
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
             </div>
 
         </div>
